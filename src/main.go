2b04ea9265ea746c1cfd7533545d2f08e09fb301
mrh929 <809999463@qq.com>
2022-03-08 21:26:24 +0800

    fix bus

--- src/main.go
+++ src/main.go
@@ -49,16 +49,19 @@ func envSet() {
 	if SRV_PASSWD == "" {
 		SRV_PASSWD = "123456!"
 	}
+	log.Info("SRV_PASSWD = ", SRV_PASSWD)
 
 	SRV_HOST = os.Getenv("SRV_HOST") // init server host
 	if SRV_HOST == "" {
 		SRV_HOST = "127.0.0.1"
 	}
+	log.Info("SRV_HOST = ", SRV_HOST)
 
 	SRV_PORT = os.Getenv("SRV_PORT") // init server port
 	if SRV_PORT == "" {
 		SRV_PORT = "8000"
 	}
+	log.Info("SRV_PORT = ", SRV_PORT)
 
 	SRV_PROTO = os.Getenv("SRV_PROTO") // init client access protocal (e.g. http/https)
 	if SRV_PROTO == "" {
@@ -66,27 +69,33 @@ func envSet() {
 	} else if SRV_PROTO != "https" && SRV_PROTO != "http" {
 		log.Fatal("server protocol not supported")
 	}
+	log.Info("SRV_PROTO = ", SRV_PROTO)
 
 	SRV_BASE_PATH = os.Getenv("SRV_BASE_PATH") // init url base path (e.g. exam.ple/foo/bar/ABCDE)
 	SRV_BASE_PATH = strings.TrimRight(SRV_BASE_PATH, "/")
+	log.Info("SRV_BASE_PATH = ", SRV_BASE_PATH)
 
 	SQL_ROOT_PASSWD = os.Getenv("SQL_ROOT_PASSWD") // init sql root password
 	if SQL_ROOT_PASSWD == "" {
 		SQL_ROOT_PASSWD = "test"
 	}
+	log.Info("SQL_ROOT_PASSWD = ", SQL_ROOT_PASSWD)
 
 	SQL_HOST = os.Getenv("SQL_HOST") // init sql server host
 	if SQL_HOST == "" {
 		SQL_HOST = "127.0.0.1"
 	}
+	log.Info("SQL_HOST = ", SQL_HOST)
 
 	SQL_PORT = os.Getenv("SQL_PORT") // init sql server port
 	if SQL_PORT == "" {
 		SQL_PORT = "3306"
 	}
+	log.Info("SQL_PORT = ", SQL_PORT)
 
 	SQL_DATABASE_NAME = os.Getenv("SQL_DATABASE_NAME") // init sql database name
 	if SQL_DATABASE_NAME == "" {
 		SQL_DATABASE_NAME = "db"
 	}
+	log.Info("SQL_DATABASE_NAME = ", SQL_DATABASE_NAME)
 }
